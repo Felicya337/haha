b9c560f26150a8bb64124b12a4cf349b36e5563c
Felicya337 <felicyapanjaitan790@gmail.com>
2024-12-23 07:37:11 +0700

    first commit

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main',['activePage' => 'home', 'titlePage'=> __('Home')])
+@section('title','Home')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <h1>Ini Halaman Utama</h1>
+        </div>
+    </div>
+@endsection
